7d74c78896372fd861c559dad200351ee1a899b6
Guangbei <guangbei.yi@wsu.edu>
2022-07-11 16:44:42 -0700

    update driver

--- JepJazzerTest.java
+++ JepJazzerTest.java
@@ -4,24 +4,47 @@ import jep.SubInterpreter;
 import jep.JepConfig;
 import java.io.File;
 
+class ResultIsNotEqualException extends Exception
+{
+    public ResultIsNotEqualException(String msg)
+    {
+        super(msg);
+    }
+}
+
+
 public class JepJazzerTest {
+
+    static File pwd;
+    static Interpreter interp;
+
+    public static void fuzzerInitialize()
+    {
+        pwd = new File(".");
+        interp = new SubInterpreter(new JepConfig().addIncludePaths(pwd.getAbsolutePath()));
+        interp.exec("import pyTest");
+    }
     
     public static void fuzzerTestOneInput(FuzzedDataProvider data) {
 
-        File pwd = new File(".");
-
-        try (Interpreter interp = new SubInterpreter(new JepConfig().addIncludePaths(pwd.getAbsolutePath()))) {
+        try {
 
             int input = data.consumeInt();
 
-            interp.exec("import pyTest");
+            
             // any of the following work, these are just pseudo-examples
         
             // using exec(String) to invoke methods
             interp.set("arg1", input);
             interp.set("arg2", input + 2);
             interp.exec("x = pyTest.calc_add(arg1, arg2)");
+            //Object output = interp.getValue("x");
             interp.getValue("x");
+
+            // if(output != (input * 2 + 2))
+            // {
+            //     throw new ResultIsNotEqualException("The Result are incorrect");
+            // }
         
         }
         catch (Exception e) 
